80f6f1fa7913e0e15db33647c3d05be9c1f88525
Apurv Kumar <kuroseiryuu025@gmail.com>
2022-09-06 16:48:32 +0530

    update

--- HTML_CSS/Practice/my.cpp
+++ HTML_CSS/Practice/my.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main(){
-    
+    int a = 5,b = 10;
+    cout << a << "," << a++ << "," << ++a << endl; // 567 X //767 O
+    a = 5;
+    cout << a << "," << ++a << "," << a++ << endl; // 566 X //775 O
+    a = 5;
+    cout<<a++ + ++a;
     return 0;
 }
